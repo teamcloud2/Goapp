5679898acf7ea0aa3c4c938c2659e31b7e7ff1ef
A R <85432029+teamcloud2@users.noreply.github.com>
2022-01-12 10:45:53 +0900

    Create goapp.go

---
+++ goapp.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+  "fmt"
+  "OS"
+  "log"
+  "net"
+  "net/http"
+  
+)
+
+func gohandler(w http.ResponseWriter, r *http.Request){
+  name, err := os.Hostname()
+  if err != nil{
+    fmt.Printf("error: %v\n", err)
+    return
+  }
+  
+  fmt.Fprintln(w, "Hostname: ", name)
+  
+  addr, err :=net.LookupHost(name)
+  if err != nil{
+    fmt.Printf("error: %v\n", err)
+    return
+  }
+  
+  fmt.Fprintln(w,"IP: ",addr)
+  
+}
+
+func main(){
+
+  fmt.Fprintln(os.Stdout, "Go !!! Go Application ......")
+  http.HandleFunc("/",gohandler)
+  log.Fatal(http.ListenAndServe(":9090",nil))
+}
